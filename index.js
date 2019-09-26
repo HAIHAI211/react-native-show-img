c9d05d2210575443f121e308244b3f2e45d0aca2
sunzheng <haihai211>
2019-09-26 18:32:07 +0800

    查看大图ios源码

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-const { RNShowImg } = NativeModules;
+// const { RNShowImg } = NativeModules;
 
-export default RNShowImg;
+// const params = {
+//     index: 0,
+//     images: [
+//         {
+//             origImage: '',
+//             editedImage: ''
+//         }
+//     ]
+// }
+
+const show = (params) => {
+    const stringParams = JSON.stringify(params)
+    if (Platform.OS === 'ios') {
+        NativeModules.RNShowImg.show(stringParams)
+    } else if (Platform.OS === 'android') {
+        NativeModules.PicEditModule.edit(stringParams)
+    }
+}
+
+export default show
--- ios/RNShowImg.m
+++ ios/RNShowImg.m
@@ -1,13 +1,45 @@
 
 #import "RNShowImg.h"
+#import "YBImageBrowser.h"
+#import "Utils.h"
+
+@interface RNShowImg()
+
+@end
 
 @implementation RNShowImg
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(show:(NSString *)json) {
+
+  NSMutableDictionary *dic = [Utils dictionaryWithJsonString:json];
+
+  NSMutableArray *images = dic[@"images"];
+
+  NSMutableArray *dataSource = [[NSMutableArray alloc] init];
+  for (NSMutableDictionary *image in images) {
+    // 本地图片
+    YBIBImageData *data = [YBIBImageData new];
+    data.imageURL = [NSURL URLWithString:image[@"origImage"]];
+    [dataSource addObject:data];
+  }
+
+  __weak typeof(self) weakSelf = self;
+  dispatch_async(dispatch_get_main_queue(), ^{
+    YBImageBrowser *browser = [YBImageBrowser new];
+    browser.dataSourceArray = dataSource;
+    browser.currentPage = [dic[@"index"] intValue];
+
+    [browser showToView:[weakSelf rootViewController].view];
+  });
 }
-RCT_EXPORT_MODULE()
+
+#pragma mark - LFPhotoEdi
+
+- (UINavigationController *)rootViewController
+{
+  return (UINavigationController *)[UIApplication sharedApplication].delegate.window.rootViewController;
+}
 
 @end
-  
